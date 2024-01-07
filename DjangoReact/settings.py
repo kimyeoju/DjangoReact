877d9db30e1c615299ae84e6008ff87ea5dc9860
kimyeoju <yeojoo0031@gmail.com>
2024-01-07 21:33:06 +0900

    feat : debug toolbar 설치 및 url, settings middleware 설정

--- DjangoReact/settings.py
+++ DjangoReact/settings.py
@@ -32,15 +32,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Apps
+    'debug_toolbar',
+    # Loclas Apps
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- DjangoReact/urls.py
+++ DjangoReact/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug__toolbar.urls)),
+    ]
+    
+    
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     

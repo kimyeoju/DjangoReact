11b6dfe12cde1705102792c6b757b00a2b56f4bf
kimyeoju <yeojoo0031@gmail.com>
2024-01-09 22:41:33 +0900

    chore : settings/common.py BASE_DIR 경로 수정

--- DjangoReact/settings/common.py
+++ DjangoReact/settings/common.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from os.path import abspath, dirname
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production

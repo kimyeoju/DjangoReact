b345f39e3de84fbad418596f9fcc121ee02e9ae6
kimyeoju <yeojoo0031@gmail.com>
2024-01-09 22:25:05 +0900

    chore : gitignore 파일 수정

--- DjangoReact/settings/common.py
+++ DjangoReact/settings/common.py
@@ -134,6 +134,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# debug시 설정
+INTERNAL_IPS = ['127.0.0.1']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- DjangoReact/urls.py
+++ DjangoReact/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
-        path('__debug__/', include(debug__toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
     ]
     
     

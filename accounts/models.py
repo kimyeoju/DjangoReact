322a8a15000542a2363a70a0c57f37939ec476a5
kimyeoju <yeojoo0031@gmail.com>
2024-01-10 23:30:41 +0900

    feat : user 모델 생성 및 signup views.py forms.py 추가

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# class User(AbstractUser):
-#     pass
+class User(AbstractUser):
+    website_url = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+
+
+class Profile(models.Model):
+    pass

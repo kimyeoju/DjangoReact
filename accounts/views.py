9b0556c631be075947bc6ae1bb83a47511e22c42
kimyeoju <yeojoo0031@gmail.com>
2024-01-11 20:22:58 +0900

    chore : signup redirect 경로 수정

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,9 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             messages.success(request, "회원가입 환영합니다.")
-            return redirect("/")
+            # '/' -> url pattern name을 적어도됨
+            next_url = request.GET.get('next', '/')
+            return redirect("next_url")
     else:
         form = SignupForm()
     return render(request, 'accounts/signup_form.html',{

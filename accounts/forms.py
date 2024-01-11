5c4b99913a9b3e5f68a1010e277832030c1b4ac0
kimyeoju <yeojoo0031@gmail.com>
2024-01-11 19:47:13 +0900

    chore : signupform fields 추가 및 필수입력 설정, layout.html message 설정

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,16 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 
-class SignupForm(forms.ModelForm):
-    class Meta:
+class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        # super를 통해 부모를 호출
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'email', 'first_name', 'last_name']
